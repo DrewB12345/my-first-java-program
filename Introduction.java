e28a8871c36b53ada8a62805708d91fc31ce36fe
Drew Bagwell <abagwe7359@greenvilleschools.us>
2017-08-25 09:56:58 -0400

    Initial sharing of project

---
+++ Introduction.java
@@ -0,0 +1,26 @@
+/**
+ * This program tells a few things about me.
+ * 
+ * @author Drew Bagwell
+ * @version August 22, 2017
+ */
+
+public class Introduction
+{
+    public static void main(String[] args)
+    {
+        // here are some facts about me
+        System.out.println("My name is Drew Bagwell.");
+        System.out.println("I was born in Greenville.");
+        
+        // now store some numbers
+        int year = 2017;
+        int age = 17;
+        
+       // What happens here?
+       System.out.println("year - age");
+       System.out.println(year - age);
+       System.out.println("I am " + age + " years old.");
+    }
+}
+        
